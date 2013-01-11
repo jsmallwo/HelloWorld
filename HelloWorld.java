7a7bb31a7372952a4301e45df12f47f8b4c2ea49
Jacob Smallwood <jsmallwo@ggc.edu>
2013-01-11 13:06:43 -0500

    made HelloWorld more interesting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		long population = 7000000000;
+		System.out.println("World population: " + population);
+		System.out.println("Launching nukes");
+		population -= 2342785691;
+		System.out.println("World population: " + population);
 	}
 }
