4a13c1262d6679dc6a4f70e142ce69aeaf8b044e
Jacob Smallwood <jsmallwo@ggc.edu>
2013-01-14 13:25:16 -0500

    SomeClass stores a value

---
+++ SomeClass.java
@@ -0,0 +1,15 @@
+public class SomeClass{
+	private long value;
+
+	public SomeClass(long l){
+		value = l;
+	}
+
+	public void set(long l){
+		value = l;
+	}
+
+	public long get(){
+		return value;
+	}
+}
